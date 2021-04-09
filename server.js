b1dacfd36683a995382a60557358706459a2bbdb
GreyFoxx <skjha60070@gmail.com>
2021-04-09 21:02:40 +0530

    Added DB configuration

--- server.js
+++ server.js
@@ -12,9 +12,22 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({limit: '10mb', extended:false}));
 
+const mongourl = "mongodb+srv://mellark201:mellark201@codecollab.zucli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const mongoose = require('mongoose');
+mongoose.connect(mongourl, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
+const db = mongoose.connection
+db.on('error', error => console.log(error));
+db.once('open', () => {
+    console.log('Connected to Mongoose');
+})
+
 
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
 server.listen(process.env.PORT || 3000);
+
+
+// mongodb+srv://shubham:shubham@123@probono-vqmwn.mongodb.net/Probono?retryWrites=true&w=majority
