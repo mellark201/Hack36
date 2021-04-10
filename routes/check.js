78ce58c8c82cc5cdfe47d6555949642e3e250e98
Gulshan Kumar <gk24015@gmail.com>
2021-04-11 00:37:00 +0530

    Team Members

--- routes/check.js
+++ routes/check.js
@@ -7,10 +7,18 @@ const log = require('../middleware');
 router.get('/:a/:b', log.isLoggedIn, async(req, res) => {
     // console.log(req.params.a);
     const proposal = await Proposals.findById(req.params.a);
+    let names= [];
     const activeUser = [];
+     
     console.log(proposal.developerId);
+    
+    const admin= await User.findById(proposal.createId);
+    const adminName= admin.name;
+    console.log(adminName);
     for(const id of proposal.developerId) {
         const tempDeveloper = await User.findById(id);
+        
+        names.push(tempDeveloper.name);
         if(tempDeveloper.isActive) {
             let temptemp = {};
             temptemp.username = tempDeveloper.username;
@@ -20,7 +28,7 @@ router.get('/:a/:b', log.isLoggedIn, async(req, res) => {
         }
     }
     if(proposal.developerId.includes(req.params.b) || proposal.createId === req.params.b)
-        res.render('check/show', {proposal: proposal, errorMessage: '', isLog: req.user !== undefined, activeusers:activeUser});
+        res.render('check/show', {adminName:adminName,names:names,proposal: proposal, errorMessage: '', isLog: req.user !== undefined, activeusers:activeUser});
     else
         res.redirect(`/proposal/${proposal.id}?er=` + encodeURIComponenet('Signup for this project first'));
 })
